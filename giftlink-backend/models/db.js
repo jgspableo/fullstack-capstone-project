2919bdadb0910a410544b5e36df6654408c48e15
jgspableo <jgspableo@outlook.com>
2026-03-31 14:27:08 +0800

    Refactor database connection logic in db.js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,20 +9,15 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+	if (dbInstance){
+		return dbInstance
+	};
 
-    const client = new MongoClient(url);      
+	const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+	await client.connect();
+	dbInstance = client.db(dbName);
+	return dbInstance;
 }
 
 module.exports = connectToDatabase;
