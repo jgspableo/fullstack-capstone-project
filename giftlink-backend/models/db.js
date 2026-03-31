cd9aaa6ed51a9abc1ff041ada0b104283c7a081f
jgspableo <jgspableo@outlook.com>
2026-03-31 16:12:25 +0800

    Refactor MongoDB connection logic in db.js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,21 @@
-// db.js
+// models/db.js
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+const { MongoClient } = require('mongodb');
 
+const url = process.env.MONGO_URL;
+const dbName = 'giftdb';
 let dbInstance = null;
-const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance) {
-        return dbInstance;
-    }
-
-    const client = new MongoClient(url);
-
-    // Task 1: Connect to MongoDB
-    await client.connect();
-
-    // Task 2: Connect to database giftdb and store in variable dbInstance
-    dbInstance = client.db(dbName);
-
-    // Task 3: Return database instance
+  if (dbInstance) {
     return dbInstance;
+  }
+
+  const client = new MongoClient(url);
+  await client.connect();
+  dbInstance = client.db(dbName);
+  return dbInstance;
 }
 
 module.exports = connectToDatabase;
